290c8a8f0e4449e6ab1aa98c785e48f4e9988696
Marianne Do <marianne.do@barrelny.com>
2014-08-22 01:05:59 -0400

    trying some production environment stuff for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,12 +85,14 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'barrelny.com',
-    user_name:            'mariannepdo@gmail.com',
-    password:             ENV['admin_pw'],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+    address: "mail.barrelny.com",
+    port: 25,
+    authentication: "plain",
+    user_name: "marianne.do@barrelny.com",
+    password: ENV['SMTP_PASSWORD'],
+    enable_starttls_auto: false
+  }
+  config.action_mailer.raise_delivery_errors = true
 end
