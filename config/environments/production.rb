e556fda21ff666fa8f6b9ffdf7d9cc069648114f
Marianne Do <marianne.do@barrelny.com>
2014-08-22 01:08:52 -0400

    trying again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.delivery_method = :sendmail
+  #config.action_mailer.delivery_method = :sendmail
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "mail.barrelny.com",
